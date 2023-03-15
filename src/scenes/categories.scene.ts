919a1504705be58cb87948c1900a94b7fa6314b6
Sergey <sergey.ar10n@gmail.com>
2023-03-15 16:11:29 +0300

    version 1.9

--- src/scenes/categories.scene.ts
+++ src/scenes/categories.scene.ts
@@ -29,17 +29,24 @@ categoriesScene.on('text', async (ctx) => {
             data: { currentCategoryId: category?.id }
         });
         await ctx.scene.enter('services');
+        await ctx.scene.leave();
+    } else if (category && category.name === 'АстроТЕСТ') {
+        await ctx.scene.enter('startTest');
+        await ctx.scene.leave();
     } else if (category && !(category.payable)) {
         await prisma.session.update({
             where: { userId },
             data: { currentCategoryId: category?.id }
         });
         await ctx.scene.enter('categoryDetails');
+        await ctx.scene.leave();
     } else if (categoryName === 'Корзина') {
         await ctx.scene.enter('cart');
+        await ctx.scene.leave();
     } else {
         ctx.reply('Пожалуйста, выберите раздел из меню.');
         await ctx.scene.enter('categories');
+        await ctx.scene.leave();
     }
 });
 
